1442bbb051098cae9bad452ea7f48c13dfb459d6
Lucio <oapi.help@gmail.com>
2015-07-19 23:36:08 -0700

    DEBUG flag

--- rbac.py
+++ rbac.py
@@ -31,6 +31,7 @@ def load_config(key):
 CLIENT_ID=load_config('CLIENT_ID')
 CLIENT_SECRET=load_config('CLIENT_SECRET')
 REDIRECT_URI=load_config('REDIRECT_URI')
+DEBUG=os.getenv('DEBUG')
 
 @app.route('/')
 def index():
@@ -73,4 +74,4 @@ def receive_code():
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=DEBUG)
